a38c9563b3e086d44f87e5caf9faf07e242bd052
Michael Carmody <mr.carmody89@gmail.com>
2021-09-15 13:20:35 -0400

    File Routes

--- routes/api.js
+++ routes/api.js
@@ -49,11 +49,11 @@ router.put("/api/Exercise/:id", (req, res) => {
 })
 
 router.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../public/exercise.html"));
+  res.sendFile(path.join(__dirname + "../public/exercise.html"));
 })
 
 router.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../public/stats.html"));
+  res.sendFile(path.join(__dirname + "../public/stats.html"));
 })
 
 module.exports = router;
